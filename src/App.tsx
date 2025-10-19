0df2ae1435852df449c3d5965f91e28f4a2db067
bit <mushbeen25@gmail.com>
2025-10-19 22:39:03 +0900

    feat: add Footer

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Hero />
       <Projects />
+      <Footer />
     </div>
   );
 }
