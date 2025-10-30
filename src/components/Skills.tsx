4c5bebcc0811250621a10c69783cdfc0091752f6
bit <mushbeen25@gmail.com>
2025-10-30 22:45:04 +0900

    feat: Added skill cards and images

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -4,12 +4,46 @@ import "./Skills.css";
 type SkillCardContent = {
   img: string;
   name: string;
+  color: string;
 };
 
 const Skills: React.FC = () => {
   const skillList: SkillCardContent[] = [
-    { img: "./assets/NullTasker.png", name: "C++" },
-    { img: "./assets/NullTasker.png", name: "Python" },
+    {
+      img: "./assets/stack-icons/cplusplus.png",
+      name: "C++",
+      color: "#00599C",
+    },
+    {
+      img: "./assets/stack-icons/python.png",
+      name: "Python",
+      color: "#3776AB",
+    },
+    {
+      img: "./assets/stack-icons/javascript.png",
+      name: "JavaScript",
+      color: "#F7DF1E",
+    },
+    {
+      img: "./assets/stack-icons/typescript.png",
+      name: "TypeScript",
+      color: "#3178C6",
+    },
+    {
+      img: "./assets/stack-icons/html5.png",
+      name: "HTML",
+      color: "#E34F26",
+    },
+    {
+      img: "./assets/stack-icons/css.png",
+      name: "CSS",
+      color: "#663399",
+    },
+    {
+      img: "./assets/stack-icons/react.png",
+      name: "React",
+      color: "#61DAFB",
+    },
   ];
   const skillCards = skillList.map((s) => {
     return <SkillCard key={s.name} props={s} />;
@@ -29,10 +63,12 @@ const Skills: React.FC = () => {
 type SkillCardProps = { props: SkillCardContent };
 
 const SkillCard: React.FC<SkillCardProps> = ({ props }) => {
-  const { img, name } = props;
+  const { img, name, color } = props;
   return (
     <div className="skill-card">
-      <img className="skill-img" src={img} alt={`${name} logo`} />
+      <div className="skill-img" style={{ background: color }}>
+        <img src={img} alt={`${name} logo`} />
+      </div>
       <h3 className="skill-name">{name}</h3>
     </div>
   );
