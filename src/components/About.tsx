2ad9d56ed683f88c6bb1fe613ef2f6a484cd3f20
bit <mushbeen25@gmail.com>
2025-11-13 10:50:27 +0900

    feat: modify styles

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,14 +5,12 @@ const About: React.FC = () => {
   return (
     <section id="about">
       <div className="section-contents">
-        <div className="section-title">
-          <h2>About Me</h2>
-        </div>
         <div className="greeting">
           <div className="greeting-image">
             <img src="./assets/kanibaPigeon.jpg" alt="kanibaPigeon"></img>
           </div>
           <div className="greeting-text">
+            <h3>Name: bit</h3>
             <p>大阪公大高専2年生のbitです。組み込みやWeb開発を行っています。</p>
             <p>最近はWebデザインに興味があったりするかも</p>
           </div>
