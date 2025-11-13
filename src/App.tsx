45e1d6a1d99c10b99262c102db3ae8b825bb8e58
bit <mushbeen25@gmail.com>
2025-11-13 11:09:25 +0900

    refac: remove unused component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
-import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
