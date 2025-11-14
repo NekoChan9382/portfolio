5f02901da612264253005f6e8303a019f5b742cf
bit <mushbeen25@gmail.com>
2025-11-14 10:32:19 +0900

    feat: add pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Top from "./components/Top";
+import TopPage from "./components/TopPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Top />} />
+        <Route path="/" element={<TopPage />} />
       </Routes>
     </BrowserRouter>
   );
