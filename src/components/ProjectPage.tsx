6b65256e12264549abd1f24e9084d1928b4148d9
bit <mushbeen25@gmail.com>
2025-11-14 10:32:36 +0900

    feat: add pages component

---
+++ src/components/ProjectPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProjectPage: React.FC = () => {
+  return <></>;
+};
+
+export default ProjectPage;
---
+++ src/components/SkillPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SkillPage: React.FC = () => {
+  return <></>;
+};
+
+export default SkillPage;
