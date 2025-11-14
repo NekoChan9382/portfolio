e3e892e23f8d35aead08120f0ddee063894c0d9a
bit <mushbeen25@gmail.com>
2025-11-14 09:37:38 +0900

    feat: categorized skill stack

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,20 +1,48 @@
 import React from "react";
-import { SkillCardContent, skillList } from "./Contents";
+import {
+  SkillCardContent,
+  skillList,
+  SkillTypeList,
+  SkillType,
+} from "./Contents";
 import "./Skills.css";
 
+type SkillSectionProps = { category: SkillType; skills: SkillCardContent[] };
+
 const Skills: React.FC = () => {
-  const skillCards = skillList.map((s) => {
-    return <SkillCard key={s.name} props={s} />;
-  });
+  const groupedSkills: SkillSectionProps[] = Object.values(SkillTypeList).map(
+    (category) => {
+      const skills = skillList.filter((skill) => skill.type === category);
+      return { category, skills };
+    }
+  );
+  const skillCards = groupedSkills.map((group) => (
+    <SkillSection key={group.category} content={group} />
+  ));
   return (
     <section id="skills">
       <div className="section-content">
         <div className="section-title">
           <h2>Skills</h2>
         </div>
-        <div className="cards">{skillCards}</div>
+        <div className="skill-list">{skillCards}</div>
       </div>
     </section>
+  );
+};
+
+const SkillSection: React.FC<{ content: SkillSectionProps }> = ({
+  content,
+}) => {
+  const { category, skills } = content;
+  const skillCards = skills.map((s) => {
+    return <SkillCard key={s.name} props={s} />;
+  });
+  return (
+    <div className="skill-field">
+      <h2 className="skill-category">{category}</h2>
+      <div className="cards">{skillCards}</div>
+    </div>
   );
 };
 
@@ -27,7 +55,6 @@ const SkillCard: React.FC<SkillCardProps> = ({ props }) => {
       <div className="skill-img" style={{ background: color }}>
         <img src={img} alt={`${name} logo`} />
       </div>
-      <h3 className="skill-name">{name}</h3>
     </div>
   );
 };
