f025214d61ca92c6359c297a25133a44a3d79616
bit <mushbeen25@gmail.com>
2025-11-17 13:11:47 +0900

    feat: add profile section

---
+++ src/components/About/Profile.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Profile.css";
+
+const Profile: React.FC = () => {
+  return (
+    <section id="profile">
+      <div className="section-content">
+        <h2>Profile</h2>
+        <p>
+          Hello! I'm a passionate developer with experience in building web
+          applications using modern technologies. I love learning new skills and
+          taking on challenges that help me grow both personally and
+          professionally.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Profile;
