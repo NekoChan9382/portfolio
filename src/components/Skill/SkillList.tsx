8020c52a796478d2f5229d586d747e866c659aac
bit <mushbeen25@gmail.com>
2025-11-19 00:11:17 +0900

    feat: add skill card

--- src/components/Skill/SkillList.tsx
+++ src/components/Skill/SkillList.tsx
@@ -5,6 +5,7 @@ import {
   SkillTypeList,
   SkillType,
 } from "../Contents";
+import { Card } from "@mui/material";
 import "./SkillList.css";
 
 type GridProps = { category: SkillType; skills: SkillCardContent[] };
@@ -16,12 +17,35 @@ const SkillList: React.FC = () => {
       return { category, skills };
     }
   );
-  return <></>;
+  const Grids = groupedSkills.map((group) => (
+    <GridItem key={group.category} content={group} />
+  ));
+  return <div>{Grids}</div>;
 };
 
 const GridItem: React.FC<{ content: GridProps }> = ({ content }) => {
   const { category, skills } = content;
-  return <div className="grid-item"></div>;
+  return (
+    <div className="grid-item">
+      <h2 className="skill-category">{category}</h2>
+      <div className="cards">
+        {skills.map((s) => (
+          <SkillCard key={s.name} props={s} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const SkillCard: React.FC<{ props: SkillCardContent }> = ({ props }) => {
+  const { img, name, color } = props;
+  return (
+    <Card className="skill-card">
+      <div className="skill-img" style={{ background: color }}>
+        <img src={`${process.env.PUBLIC_URL}${img}`} alt={`${name} logo`} />
+      </div>
+    </Card>
+  );
 };
 
 export default SkillList;
