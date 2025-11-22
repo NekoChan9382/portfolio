7491102f9151f350ffb111d9d24fb2e089286d12
bit <mushbeen25@gmail.com>
2025-11-22 15:40:54 +0900

    feat: add about section link

--- src/components/Top/About.tsx
+++ src/components/Top/About.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Section from "../Common/Section";
 import "./About.css";
+import { Link } from "react-router-dom";
+import { borderColor, hoverColor } from "../Common/Contents";
+import { motion } from "motion/react";
 
 const About: React.FC = () => {
   return (
@@ -10,12 +13,27 @@ const About: React.FC = () => {
           <img
             src={`${process.env.PUBLIC_URL}/assets/kanibaPigeon.jpg`}
             alt="kanibaPigeon"
-          ></img>
+          />
         </div>
         <div className="greeting-text">
           <h3>Name: bit</h3>
           <p>大阪公大高専2年生のbitです。組み込みやWeb開発を行っています。</p>
           <p>最近はWebデザインに興味があったりするかも</p>
+          <Link to="/about">
+            <motion.div
+              className="about-link-btn"
+              initial={{
+                borderColor: borderColor,
+              }}
+              whileHover={{
+                borderColor: hoverColor,
+                boxShadow: `0 0 20px ${hoverColor}40`,
+              }}
+              transition={{ duration: 0.3, ease: "easeOut" }}
+            >
+              View Profile
+            </motion.div>
+          </Link>
         </div>
       </div>
     </Section>
