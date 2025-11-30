b7b896490654d8f6bd9c023692d294869c885423
bit <mushbeen25@gmail.com>
2025-11-30 23:00:33 +0900

    chore: add Helper function file

---
+++ src/shared/utils/Helper.ts
@@ -0,0 +1,24 @@
+import { SkillTypeList, GroupedSkills } from "../types/Types";
+import { skillList } from "../data/Contents";
+import { useMediaQuery } from "react-responsive";
+export const SkillGroups: GroupedSkills[] = Object.values(SkillTypeList).map(
+  (category) => ({
+    category,
+    skills: skillList.filter((skill) => skill.type === category),
+  })
+);
+
+export const getCSSVariable = (variableName: string): string => {
+  return getComputedStyle(document.documentElement)
+    .getPropertyValue(variableName)
+    .trim();
+};
+
+export const getBorderColor = () => getCSSVariable("--border-color");
+export const getHoverColor = () => getCSSVariable("--hover-color");
+
+const mobileWidth: number = 768;
+
+export const useIsMobile = (): boolean => {
+  return useMediaQuery({ query: `(max-width: ${mobileWidth}px)` });
+};
